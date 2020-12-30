34142fe763277267baeb5e8e191ae1fb5d605b75
Iuri Ferreira <76744994+devappbr@users.noreply.github.com>
2020-12-30 04:25:37 -0300

    Update conn.js

--- src/services/apiRest/conn.js
+++ src/services/apiRest/conn.js
@@ -1,4 +1,4 @@
-const baseURL = "http://3cashuser-dev.us-east-2.elasticbeanstalk.com";
+const baseURL = "http://ec2-15-228-50-13.sa-east-1.compute.amazonaws.com:3000/";
 const baseLocal = 'http://ec2-15-228-50-13.sa-east-1.compute.amazonaws.com:3000/'
 
 
