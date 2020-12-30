ce4cd81210d8ade12db9c02ae6061b2ef9956ccc
Iuri Ferreira <76744994+devappbr@users.noreply.github.com>
2020-12-30 04:05:15 -0300

    Correcao para AWS

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/"
+  baseURL: "http://ec2-15-228-50-13.sa-east-1.compute.amazonaws.com:3000/"
 });
 
 api.interceptors.request.use(async config => {
--- src/services/apiRest/conn.js
+++ src/services/apiRest/conn.js
@@ -1,5 +1,5 @@
 const baseURL = "http://3cashuser-dev.us-east-2.elasticbeanstalk.com";
-const baseLocal = 'http://localhost:3000/'
+const baseLocal = 'http://ec2-15-228-50-13.sa-east-1.compute.amazonaws.com:3000/'
 
 
 
